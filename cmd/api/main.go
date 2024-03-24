542c1b51f2646a67d669fe41e58019af16ec5b6d
Iivari Leinonen <iivari.leinonen@gmail.com>
2024-03-24 09:29:18 +0200

    update main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+// global constant holding the version of the app
+const version = "1.0.0"
+
+// config struct to hold the configuration of the app
+type config struct {
+	port int
+	env  string
+}
+
+// application struct to hold the dependencies
+// for the HTTP handlers, helpers, and middleware
+type application struct {
+	config config
+	logger *slog.Logger
+}
 
 func main() {
-  fmt.Println("Hello world!")
+	// declare instance of config
+	var cfg config
+
+	// read port and env from the command line
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	// init logger for writing to stdout
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// declare app instance
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// servemux and routing
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// declare http server
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+  // start server
+  logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
+  err := srv.ListenAndServe()
+  logger.Error(err.Error())
+  os.Exit(1)
 }
