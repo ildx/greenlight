01b9c0b3b157b67c31536452d9cbf92232aa1ce0
Iivari Leinonen <iivari.leinonen@gmail.com>
2024-03-24 16:19:26 +0200

    format and envelope responses

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -11,34 +11,37 @@ import (
 
 // get "id" from request context
 func (app *application) readIDParam(r *http.Request) (int64, error) {
-  params := httprouter.ParamsFromContext(r.Context())
+	params := httprouter.ParamsFromContext(r.Context())
 
-  id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-  if err != nil || id < 1 {
-    return 0, errors.New("invalid id parameter")
-  }
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
 
-  return id, nil
+	return id, nil
 }
 
+// envolope type for wrapping responses
+type envelope map[string]any
+
 // write json to responses
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
-  j, err := json.Marshal(data)
-  if err != nil {
-    return err
-  }
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	j, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
 
-  // terminal output niceness
-  j = append(j, '\n')
+	// terminal output niceness
+	j = append(j, '\n')
 
-  // write headers
-  for key, value := range headers {
-    w.Header()[key] = value
-  }
+	// write headers
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
 
-  w.Header().Set("Content-Type", "application/json")
-  w.WriteHeader(status)
-  w.Write(j)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(j)
 
-  return nil
+	return nil
 }
