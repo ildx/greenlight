15502e361f97544069f31c4b105d100c5a8ee734
Iivari Leinonen <iivari.leinonen@gmail.com>
2024-03-26 09:26:59 +0200

    validate query string parameters

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -179,9 +179,9 @@ func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Reques
 
 func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title    string
-		Genres   []string
-    data.Filters
+		Title  string
+		Genres []string
+		data.Filters
 	}
 
 	v := validator.New()
@@ -192,12 +192,13 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 
 	input.Filters.Page = app.readInt(qs, "page", 1, v)
 	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
-  input.Filters.Sort = app.readString(qs, "sort", "id")
+	input.Filters.Sort = app.readString(qs, "sort", "id")
+	input.Filters.SortSafeList = []string{"id", "title", "year", "runtime", "-id", "-title", "-year", "-runtime"}
 
-  if !v.Valid() {
-    app.failedValidationResponse(w, r, v.Errors)
-    return
-  }
+	if data.ValidateFilters(v, input.Filters); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
 
-  fmt.Fprintf(w, "%+v\n", input)
+	fmt.Fprintf(w, "%+v\n", input)
 }
