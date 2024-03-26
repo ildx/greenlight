f858a09a741fb60063e579f2ae9d745741337428
Iivari Leinonen <iivari.leinonen@gmail.com>
2024-03-26 09:53:46 +0200

    filter lists

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -80,12 +80,14 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 	query := `
     SELECT id, created_at, title, year, runtime, genres, version
     FROM movies
+    WHERE (LOWER(title) = LOWER($1) OR $1 = '')
+    AND (genres @> $2 OR $2 = '{}')
     ORDER BY id`
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	rows, err := m.DB.QueryContext(ctx, query)
+	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
 	if err != nil {
 		return nil, err
 	}
@@ -113,7 +115,7 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 		movies = append(movies, &movie)
 	}
 
-  // check if any errors occured during iteration
+	// check if any errors occured during iteration
 	if err = rows.Err(); err != nil {
 		return nil, err
 	}
