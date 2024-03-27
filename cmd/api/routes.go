0dba33b8b98411f8b604065ee80fec7e107ff3df
Iivari Leinonen <iivari.leinonen@gmail.com>
2024-03-27 10:57:34 +0200

    activate users

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -24,6 +24,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+  router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
 	// recovery must be first, so we handle rate limit panics also
 	return app.recoverPanic(app.rateLimit(router))
