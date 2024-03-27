a3110217492c1f930c8643711437a91beaeaa206
Iivari Leinonen <iivari.leinonen@gmail.com>
2024-03-27 14:13:57 +0200

    remove debug printing

--- cmd/api/tokens.go
+++ cmd/api/tokens.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"net/http"
 	"time"
 
@@ -32,12 +31,9 @@ func (app *application) createAuthenticationTokenHandler(w http.ResponseWriter, 
 		return
 	}
 
-	fmt.Println("input", input)
-
 	// find user by email
 	user, err := app.models.Users.GetByEmail(input.Email)
 	if err != nil {
-		fmt.Println("find user error", err)
 		switch {
 		case errors.Is(err, data.ErrRecordNotFound):
 			app.invalidCredentialsResponse(w, r)
@@ -60,17 +56,12 @@ func (app *application) createAuthenticationTokenHandler(w http.ResponseWriter, 
 		return
 	}
 
-	fmt.Println("all good, next handle tokens...")
-
 	// correct password, generate 24-hour token with scope of "authentication"
 	token, err := app.models.Tokens.New(user.ID, 24*time.Hour, data.ScopeAuthentication)
 	if err != nil {
-		fmt.Println("BOO token error")
 		app.serverErrorResponse(w, r, err)
 		return
 	}
-
-	fmt.Println("all good?")
 
 	// encode token to JSON and send it to client
 	err = app.writeJSON(w, http.StatusCreated, envelope{"authentication_token": token}, nil)
