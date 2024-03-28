ae6e7140ecadd2b1b818cf069422946046c980c5
Iivari Leinonen <iivari.leinonen@gmail.com>
2024-03-28 09:31:16 +0200

    expose metrics with expvar

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -27,6 +28,8 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+
+	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
 
 	// recovery must be first, so we can handle all panics
 	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
